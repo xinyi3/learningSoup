0828c30b136a96e3910834529ab294988628335b
xinyi3 <xinyi3@ualberta.ca>
2017-07-10 17:32:48 -0600

    now goes to each individual post and checks all the contents in there as well

--- battleriteKeys.py
+++ battleriteKeys.py
@@ -4,31 +4,53 @@ import unicodedata
 import time
 from random import randint
 import smtplib
+import re
 
 set1 = {1}
-# email notification
-server = smtplib.SMTP('smtp.live.com', 587)
-server.ehlo()
-server.starttls()
-server.login("yourEmail", "YourPassword")
+### email notification
+# server = smtplib.SMTP('smtp.live.com', 587)
+# server.ehlo()
+# server.starttls()
+# server.login("yourEmail", "YourPassword")
+
+listOfLinks = []
 
 while(1):
-    headers = { 'User-Agent' : 'battlerite lite keys bot 0.1' }
+    headers = { 'User-Agent' : 'battlerite lite keys bot 0.2' }
 
-    req = urllib2.Request('https://www.reddit.com/r/BattleRite/search?q=key%7Clite&restrict_sr=on&sort=new&t=hour', None, headers)
+    req = urllib2.Request('https://www.reddit.com/r/BattleRite/search?q=key%7Clite&restrict_sr=on&sort=new&t=week', None, headers)
 
     battleriteSource = urllib2.urlopen(req).read()
     brSoup = bs.BeautifulSoup(battleriteSource, 'lxml')
 
-    body = brSoup.find(class_="listing search-result-listing")
+    body = brSoup.find(class_="contents")
 
-    for paragraph in body.find_all('p'):
-        temp = paragraph.text
-        if(temp.count('-')>1 and not(temp in set1)):
-            set1.add(temp)
-            print (temp)
-            msg = temp
-            # server.sendmail("toMailAccount", "FromMailAccount", msg.encode('utf-8'))
+    for url in body.find_all('a'):
+        postLink = url.get('href')
+        if(postLink.count('comments')>0 and postLink.count('reddit.com')>0):
+            if(postLink not in listOfLinks):
+                listOfLinks.append(postLink)
+            
+    print('found ' + str(len(listOfLinks)) + " related posts")
+    i=1
+    # make function for this
+    for link in listOfLinks:
+        print('checking post number ' + str(i))
+        i=i+1
+        req1 = urllib2.Request(link, None, headers)
+        postSource = urllib2.urlopen(req1).read()
+        postSoup = bs.BeautifulSoup(postSource, 'lxml')
+        postContent = postSoup.body
 
+        for paragraph in postContent.find_all('p'):
+            temp = paragraph.text
+            matchObj = re.match(".{5}-.{5}-.{5}", temp, flags=0)
+            if( matchObj and not(temp in set1)):
+                set1.add(temp)
+                print (temp)
+                msg = temp
+                # server.sendmail("toMailAccount", "FromMailAccount", msg.encode('utf-8'))
+        time.sleep(randint(4,7))
+        
     print('--------------------------------------------')
-    time.sleep(randint(4,7))
+    
